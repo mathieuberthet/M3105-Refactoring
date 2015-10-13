8837e14e737dd1cccfb725fbc2221d8dbeb34fa1
moi <moi@moi.com>
2015-10-13 17:30:02 +0200

    simplify comment

--- ageOfAntHill-master/projet/src/javaproject_ageOfAntHill/entity/buildable/House.java
+++ ageOfAntHill-master/projet/src/javaproject_ageOfAntHill/entity/buildable/House.java
@@ -7,16 +7,10 @@ package javaproject_ageOfAntHill.entity.buildable;
  */
 public class House extends Building {
 	/**
-	  * Just like a serial name, it's used to identify an House's type
+	  *  House's informations
 	  */
 	 private final static String HOUSE_TYPE = "HOUSE";
-	 /**
-	  * Define the House's HP
-	  */
 	 private final static int DEFAULT_HOUSE_HP = 250;
-	 /**
-	  * Define the House's ARMOR
-	  */
 	 private final static int DEFAULT_HOUSE_ARMOR = 20;
 	 
 	public House(){
