cdbb34b057cf7a1197c081156009a28a2e01a83f
moi <moi@moi.com>
2015-10-13 17:28:45 +0200

    simplify comment

--- ageOfAntHill-master/projet/src/javaproject_ageOfAntHill/entity/buildable/AntHill.java
+++ ageOfAntHill-master/projet/src/javaproject_ageOfAntHill/entity/buildable/AntHill.java
@@ -9,18 +9,14 @@ package javaproject_ageOfAntHill.entity.buildable;
 
 public class AntHill extends Building {
 	/**
-	  * Just like a serial name, it's used to identify an anthill's type
+	  * AntHill's informations (HP, armor)
 	  */
 	 private final static String ANTHILL_TYPE = "AHILL";
-	 /**
-	  * Define the Anthill's HP
-	  */
 	 private final static int ANTHILL_HP = 1000;
-	 /**
-	  * Define the Anthill's ARMOR
-	  */
 	 private final static int DEFAULT_ANTHILL_ARMOR = 10;
+	 
 	/**
+	 * 
 	 * antHill's builder 
 	 */
 	public AntHill(){
